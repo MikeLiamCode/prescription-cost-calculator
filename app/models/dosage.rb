1af0d2654395bf2fa0fec318e8046c15588d012d
MikeLiamCode <devlivelymike@gmail.com>
2025-02-26 05:46:10 +0500

    Added a method `frequency_multiplier` to calculate dosage frequency dynamically

--- app/models/dosage.rb
+++ app/models/dosage.rb
@@ -1,6 +1,19 @@
 class Dosage < ApplicationRecord
   belongs_to :medication
+  enum :frequency, { once_daily: 1, twice_daily: 2, thrice_daily: 3, once_week: 3, twice_week: 4, thrice_week: 5 }
   validates :dosage_amount, presence: true
   validates :frequency, presence: true
   validates :default_duration, presence: true, numericality: { greater_than: 0 }
+
+  def frequency_multiplier
+    case frequency
+    when 'once_daily' then 1
+    when 'twice_daily' then 2
+    when 'thrice_daily' then 3
+    when 'once_week' then 1.0 / 7
+    when 'twice_week' then 2.0 / 7
+    when 'thrice_week' then 3.0 / 7
+    else 1
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,12 @@ panadol = Medication.create(name: 'Panadol', unit_price: 7.0)
 evion = Medication.create(name: 'Evion', unit_price: 20.0)
 
 # Seed Dosages
-Dosage.create(medication: atorvastatin, dosage_amount: '2.5 mg', frequency: 'once daily', default_duration: 14)
-Dosage.create(medication: atorvastatin, dosage_amount: '5 mg', frequency: 'twice daily', default_duration: 7)
-Dosage.create(medication: metformin, dosage_amount: '500 mg', frequency: 'once daily', default_duration: 30)
-Dosage.create(medication: panadol, dosage_amount: '500 mg', frequency: 'once week', default_duration: 21)
-Dosage.create(medication: panadol, dosage_amount: '600 mg', frequency: 'once week', default_duration: 21)
-Dosage.create(medication: evion, dosage_amount: '250 mg', frequency: 'twice week', default_duration: 30)
+Dosage.create(medication: atorvastatin, dosage_amount: '2.5 mg', frequency: 'once_daily', default_duration: 14)
+Dosage.create(medication: atorvastatin, dosage_amount: '5 mg', frequency: 'twice_daily', default_duration: 7)
+Dosage.create(medication: metformin, dosage_amount: '500 mg', frequency: 'once_daily', default_duration: 30)
+Dosage.create(medication: panadol, dosage_amount: '500 mg', frequency: 'once_week', default_duration: 21)
+Dosage.create(medication: panadol, dosage_amount: '600 mg', frequency: 'once_week', default_duration: 21)
+Dosage.create(medication: evion, dosage_amount: '250 mg', frequency: 'twice_week', default_duration: 30)
 
 
 # Create a Prescription
